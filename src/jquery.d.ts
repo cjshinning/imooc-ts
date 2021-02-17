d7eda0d023131a8157d181063acfe5cea24f3cab
cjshinningg <784319417@qq.com>
2021-02-17 22:21:55 +0800

    4-12 描述文件中的全局类型（下）

--- src/jquery.d.ts
+++ src/jquery.d.ts
@@ -2,10 +2,24 @@
 // declare var $: (param: () => void) => void;
 
 // 定义全局函数
-interface JqueryInstance {
-  html: (html: string) => {};
+// interface JqueryInstance {
+//   html: (html: string) => JqueryInstance;
+// }
+
+// 函数重载
+declare function $(readyFunc: () => void): void;
+declare function $(selector: string): JqueryInstance;
+
+// 如何对对象进行类型定义，以及对类进行类型定义，以及命名空间的嵌套
+declare namespace $ {
+  namespace fn {
+    class init {}
+  }
 }
 
-declare function $(param: () => void): void;
-
-declare function $(selector: string): JqueryInstance;
+// 使用了interface的语法，实现函数重载
+// interface JQuery {
+//   (readyFunc: () => void): void;
+//   (selector: string): JqueryInstance;
+// }
+// declare var $: JQuery;
