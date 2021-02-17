b8e1d45c921dd0f4bd1e335acee228acebcc7813
cjshinningg <784319417@qq.com>
2021-02-17 22:51:50 +0800

    4-13 模块代码的类型描述文件

--- src/jquery.d.ts
+++ src/jquery.d.ts
@@ -1,25 +1,16 @@
-// 定义全局变量
-// declare var $: (param: () => void) => void;
-
-// 定义全局函数
-// interface JqueryInstance {
-//   html: (html: string) => JqueryInstance;
-// }
-
-// 函数重载
-declare function $(readyFunc: () => void): void;
-declare function $(selector: string): JqueryInstance;
-
-// 如何对对象进行类型定义，以及对类进行类型定义，以及命名空间的嵌套
-declare namespace $ {
-  namespace fn {
-    class init {}
+// ES6模块化
+declare module 'jquery' {
+  interface JqueryInstance {
+    html: (html: string) => JqueryInstance;
   }
-}
 
-// 使用了interface的语法，实现函数重载
-// interface JQuery {
-//   (readyFunc: () => void): void;
-//   (selector: string): JqueryInstance;
-// }
-// declare var $: JQuery;
+  // 混合类型
+  function $(readyFunc: () => void): void;
+  function $(selector: string): JqueryInstance;
+  namespace $ {
+    namespace fn {
+      class init {}
+    }
+  }
+  export = $;
+}
