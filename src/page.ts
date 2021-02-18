7e3548412cab6acf1fd09042f0b6bb30f11d6ab6
cjshinningg <784319417@qq.com>
2021-02-18 12:41:19 +0800

    4-14 泛型中keyof语法使用

--- src/page.ts
+++ src/page.ts
@@ -1,6 +1,21 @@
-import $ from 'jquery';
+interface Person {
+  name: string;
+  age: number;
+  gender: string;
+}
 
-$(function() {
-  $('body').html('<div>123</div>');
-  new $.fn.init();
+class Teacher {
+  constructor(private info: Person) {}
+  getInfo<T extends keyof Person>(key: T): Person[T] {
+    return this.info[key];
+  }
+}
+
+const teacher = new Teacher({
+  name: 'dell',
+  age: 18,
+  gender: 'male'
 });
+
+const test = teacher.getInfo('gender');
+console.log(test);
